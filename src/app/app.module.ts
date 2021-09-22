2e849904ca274041b9a7bf743ab7fd200a4d088a
darvinKumar <darwin.kumar@iiht.com>
2021-09-22 21:34:42 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { VerifyComponent } from './pages/verify/verify.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VerifyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
