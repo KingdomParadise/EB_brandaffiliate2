cbdd31be116fd7951fe1b395643ce95ad62c182c
darvinKumar <=darwin4416@gmail.com>
2021-09-30 00:30:49 +0530

    added form validation in verify page

--- src/app/pages/verify/verify.component.ts
+++ src/app/pages/verify/verify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./verify.component.css']
 })
 export class VerifyComponent implements OnInit{
-
-  constructor(public router: Router){
+  otpForm: FormGroup;
+  constructor(public router: Router, public _formBuilder:FormBuilder){
 
   }
   ngOnInit(){
-
+    this.otpForm = this._formBuilder.group({
+      otp: [null, Validators.required],
+    });
   }
   onSubmit(){
     this.router.navigateByUrl('/review');
